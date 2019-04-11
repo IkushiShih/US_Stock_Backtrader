d424cf4355ded4a3f0544f2be40c29cffdf9c729
IkushiShih <shihyuchih19@gapp.nthu.edu.tw>
2019-04-11 13:50:11 +0900

    Added a variation for KDJ index

--- Finviz_Screener_Scraper.py
+++ Finviz_Screener_Scraper.py
@@ -9,14 +9,14 @@ def scrape():
     import requests
     from bs4 import BeautifulSoup
 
-    req = requests.get("https://finviz.com/screener.ashx?v=111&f=sh_curvol_o1000,sh_price_u30,sh_relvol_o1.5")
+    req = requests.get("https://finviz.com/screener.ashx?v=111&f=sh_curvol_o1000,sh_price_u5,sh_relvol_o1.5&ft=3&o=-volume")
     soup = BeautifulSoup(req.content, 'html.parser')
     pages = int(soup.find_all("a", {"class": "screener-pages"})[-1].text)
 
     passing_stock = []
     for i in range(pages):
         page_idx = i*20 + 1
-        req = requests.get("https://finviz.com/screener.ashx?v=111&f=sh_curvol_o1000,sh_price_u30,sh_relvol_o1.5&r={}".format(str(page_idx)))
+        req = requests.get("https://finviz.com/screener.ashx?v=111&f=sh_curvol_o1000,sh_price_u5,sh_relvol_o1.5&ft=3&o=-volume&r={}".format(str(page_idx)))
         soup = BeautifulSoup(req.content, 'html.parser')
         tickers = soup.find_all('a',{'class':'screener-link-primary'})
         for t in tickers:
